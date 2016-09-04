25f68e8569104f3e154174ab7ecc70e2af501f9f
xautshuanglong <xjshuanglong@126.com>
2016-09-04 23:32:32 +0800

    add logger tools

--- ServiceTest/src/com/shuanglong/servicetest/MainActivity.java
+++ ServiceTest/src/com/shuanglong/servicetest/MainActivity.java
@@ -1,32 +1,41 @@
 package com.shuanglong.servicetest;
 
+import com.shuanglong.logger.Logger;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.activity_main);
+		Logger.init().hideThreadInfo();
+		Logger.t("Tag").d("Inside onCreate ...");
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings)
+		{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
